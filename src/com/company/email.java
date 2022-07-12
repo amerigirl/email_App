b88c639ae4158a08631c27cbe281554675270b5f
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-07-11 19:00:09 -0500

    Initial commit

---
+++ src/com/company/email.java
@@ -0,0 +1,49 @@
+package com.company;
+
+public class email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private String alternateEmailAddress;
+    private int mailBoxCapacity;
+
+
+    //constructor for first and last name
+    email(String firstName, String lastName, String password) {
+
+    }
+
+    //generate a random password
+
+    //ask for the department from the user
+
+    //set alternate email address
+
+    //set mailBoxCapacity
+
+    //change password capabilities
+
+    public String getName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAlternateEmailAddress(){
+        return alternateEmailAddress;
+    }
+
+    public int getMailBoxCapacity() {
+        return mailBoxCapacity;
+    }
+
+
+}
