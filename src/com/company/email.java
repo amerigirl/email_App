669e96415b898b633f4b01495bcb36acae072912
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-07-11 19:28:40 -0500

    made the setDepartment method and tested it

--- src/com/company/email.java
+++ src/com/company/email.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class email {
 
     private String firstName;
@@ -11,13 +13,33 @@ public class email {
 
 
     //constructor for first and last name
-    email(String firstName, String lastName, String password) {
+    email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = setDepartment();
+        // constructor check: System.out.println("\nEMAIL CREATED: " + this.firstName + " " + this.lastName);
+    }
 
+    //ask for the department from the user
+
+    private String setDepartment() {
+        System.out.print("\nThank you for using our email generator application! \nTo begin, please enter the number of the department that you are currently working in: \n\n1. Sales \n2. Development \n3. Accounting \n0. None/not applicable ");
+
+        Scanner in = new Scanner(System.in);
+        int deptChoice = in.nextInt();
+
+        if (deptChoice == 1) {
+            return "sales";
+        } else if (deptChoice == 2) {
+            return "development";
+        } else if (deptChoice == 3) {
+            return "accounting";
+        } else {
+            return "";
+        }
     }
 
     //generate a random password
-
-    //ask for the department from the user
 
     //set alternate email address
 
--- src/com/company/emailApp.java
+++ src/com/company/emailApp.java
@@ -1,8 +1,10 @@
 package com.company;
 
-public class emailApp {
+public class  emailApp {
 
     public static void main(String[] args) {
-	// write your code here
+    email newEmail = new email("Be-Asia", "McKerracher");
+
     }
+
 }
